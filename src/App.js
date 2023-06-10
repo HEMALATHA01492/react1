e5ed2f0584925ce050452d8045a800565845c80b
HEMALATHA01492 <hema01492@gmail.com>
2023-06-10 20:41:08 +0530

    Bootstrap Cards

--- src/App.js
+++ src/App.js
@@ -1,43 +1,96 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-// const App = ()=> {
-
-//   //javascript
-//   console.log('Hello from Component');
-//   const  now=new Date();
-//   const a=30;
-//   const b=20;
-//   console.log(now, a+b);
-//   return (
-//     <div>
-//       <p>Hello World, it is {now.toString()}</p>
-//       <p>
-//         {a} plus {b} is {a+b}
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React from 'react';
-function Hello(props){
-  // console.log(props);
-  return(
-    <div>
-      <p>Hello { props.name } , you are {props.age} years old.</p>
-    </div>
-  )
-}
-
-function App() {
+const  App=() => {
   return (
     <div>
-      <h1>Greeting</h1>
-      <Hello name='Lakshan' age={3+5}/>
-      <Hello name='Nivin' age={5+1}/>
+    <h1 class="text-center m-4">Bootstrap Cards</h1>
+    <div class="container p-5 m-5 bg-primary rounded">
+    <div class="row d-flex p-2">
+      {/* First Card */}
 
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0 d-flex">
+          <div class="card-body">
+            <h5 class="text-muted text-center">FREE</h5>
+            <h4 class="text-center">$0/month</h4>
+            <hr></hr>
+            <ul class="p-2 m-3">
+              <li class="p-2"><b>Single User</b></li>
+              <li class="p-2"><b>5GB Storage</b></li>
+              <li class="p-2"><b>Unlimited Public Projects</b></li>
+              <li class="p-2"><b>Community Access</b></li>
+              <li class="p-2 text-muted">Unlimited Private Projects</li>
+              <li class="p-2 text-muted">Dedicated Phone Support</li>
+              <li class="p-2 text-muted">Free Subdomain</li>
+              <li class="p-2 text-muted">Monthly Status Reports</li>
+           </ul>
+            <div class="d-grid">
+              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      {/* Second Card */}
+
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0 d-flex">
+          <div class="card-body">
+            <h5 class="text-muted text-center">PLUS</h5>
+            <h4 class="text-center">$9/month</h4>
+            <hr></hr>
+            <ul class="p-2 m-3">
+              <li class="p-2"><b>5 User</b></li>
+              <li class="p-2"><b>50GB Storage</b></li>
+              <li class="p-2"><b>Unlimited Public Projects</b></li>
+              <li class="p-2"><b>Community Access</b></li>
+              <li class="p-2"><b>Unlimited Private Projects</b></li>
+              <li class="p-2"><b>Dedicated Phone Support</b></li>
+              <li class="p-2"><b>Free Subdomain</b></li>
+              <li class="p-2 text-muted">Monthly Status Reports</li>
+           </ul>
+            <div class="d-grid">
+              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+{/* Third Card */}
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0 d-flex">
+          <div class="card-body">
+            <h5 class="text-muted text-center">PRO</h5>
+            <h4 class="text-center">$49/month</h4>
+            <hr></hr>
+            <ul class="p-2 m-3">
+              <li class="p-2"><b><em><strong>Unlimited</strong></em> User</b></li>
+              <li class="p-2"><b>150GB Storage</b></li>
+              <li class="p-2"><b>Unlimited Public Projects</b></li>
+              <li class="p-2"><b>Community Access</b></li>
+              <li class="p-2"><b>Unlimited Private Projects</b></li>
+              <li class="p-2"><b>Dedicated Phone Support</b></li>
+              <li class="p-2"><b><em><strong>Unlimited</strong></em> Free Subdomain</b></li>
+              <li class="p-2"><b>Monthly Status Reports</b></li>
+           </ul>
+            <div class="d-grid">
+              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+   </div>
+   </div>
   )
 }
 
-export default App
+export default App;
+
+
+
 
