e930dff37dbb57efd426714deeae2048a08c040b
HEMALATHA01492 <hema01492@gmail.com>
2023-06-28 22:30:43 +0530

    Price Card Bootstrap Task

--- src/App.js
+++ src/App.js
@@ -1,96 +1,26 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import Card1 from './Card1';
+import Card2 from './Card2';
+import Card3 from './Card3';
 
-const  App=() => {
+function App() {
   return (
     <div>
-    <h1 class="text-center m-4">Bootstrap Cards</h1>
-    <div class="container p-5 m-5 bg-primary rounded">
-    <div class="row d-flex p-2">
-      {/* First Card */}
-
-      <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0 d-flex">
-          <div class="card-body">
-            <h5 class="text-muted text-center">FREE</h5>
-            <h4 class="text-center">$0/month</h4>
-            <hr></hr>
-            <ul class="p-2 m-3">
-              <li class="p-2"><b>Single User</b></li>
-              <li class="p-2"><b>5GB Storage</b></li>
-              <li class="p-2"><b>Unlimited Public Projects</b></li>
-              <li class="p-2"><b>Community Access</b></li>
-              <li class="p-2 text-muted">Unlimited Private Projects</li>
-              <li class="p-2 text-muted">Dedicated Phone Support</li>
-              <li class="p-2 text-muted">Free Subdomain</li>
-              <li class="p-2 text-muted">Monthly Status Reports</li>
-           </ul>
-            <div class="d-grid">
-              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-      {/* Second Card */}
-
-      <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0 d-flex">
-          <div class="card-body">
-            <h5 class="text-muted text-center">PLUS</h5>
-            <h4 class="text-center">$9/month</h4>
-            <hr></hr>
-            <ul class="p-2 m-3">
-              <li class="p-2"><b>5 User</b></li>
-              <li class="p-2"><b>50GB Storage</b></li>
-              <li class="p-2"><b>Unlimited Public Projects</b></li>
-              <li class="p-2"><b>Community Access</b></li>
-              <li class="p-2"><b>Unlimited Private Projects</b></li>
-              <li class="p-2"><b>Dedicated Phone Support</b></li>
-              <li class="p-2"><b>Free Subdomain</b></li>
-              <li class="p-2 text-muted">Monthly Status Reports</li>
-           </ul>
-            <div class="d-grid">
-              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
-
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-{/* Third Card */}
-      <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0 d-flex">
-          <div class="card-body">
-            <h5 class="text-muted text-center">PRO</h5>
-            <h4 class="text-center">$49/month</h4>
-            <hr></hr>
-            <ul class="p-2 m-3">
-              <li class="p-2"><b><em><strong>Unlimited</strong></em> User</b></li>
-              <li class="p-2"><b>150GB Storage</b></li>
-              <li class="p-2"><b>Unlimited Public Projects</b></li>
-              <li class="p-2"><b>Community Access</b></li>
-              <li class="p-2"><b>Unlimited Private Projects</b></li>
-              <li class="p-2"><b>Dedicated Phone Support</b></li>
-              <li class="p-2"><b><em><strong>Unlimited</strong></em> Free Subdomain</b></li>
-              <li class="p-2"><b>Monthly Status Reports</b></li>
-           </ul>
-            <div class="d-grid">
-              <button type="button" class="btn btn-primary btn-lg">Subscribe</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-   </div>
-   </div>
+      <h2 className="text-center p-3">Task 1 - Price cart bootstrap</h2>
+    <section className="pricing py-5">
+      <div className="container">
+       <div className="row">
+          <Card1 />
+          <Card2 />
+          <Card3 />
+       </div>
+     </div>
+    </section>
+  </div>
   )
 }
 
 export default App;
-
-
-
-
